7610de40902661c556d3e88bde418ba2e7532349
Tild720 <gloveno123@gmail.com>
2025-09-01 23:00:11 +0900

    Add : Cuttable

---
+++ Assets/Works/Tild/Code/CutableObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Works.Tild.Code
+{
+    public class CutableObject : MonoBehaviour
+    {   
+        [SerializeField] GameObject cuttedPrefab;
+        [field:SerializeField] public int Health { get; private set; }
+            
+        public void Cutting(Vector3 hit)
+        {
+            Health--;
+            if (Health > 0)
+            {
+                
+                Instantiate(cuttedPrefab, hit, Quaternion.identity);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            
+            
+        }
+    }
+}
---
+++ Assets/Works/Tild/Code/MouseLock.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    {
+      
+    }
+}
