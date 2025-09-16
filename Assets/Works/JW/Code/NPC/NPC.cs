65e3b9b0f42e930b671dcef29a6cc008dea35ed9
jiwon0612 <iololloli9976@gmail.com>
2025-09-16 12:35:09 +0900

    feat:NPC NPCConversationCompo

---
+++ Assets/Works/JW/Code/NPC/NPC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+namespace Code.NPC
+{
+    
+    
+    public class NPC : MonoBehaviour
+    {
+        private NavMeshAgent _agent;
+        [SerializeField] private NPCConversationCompo  _conversation;
+
+        [SerializeField] private string testLineType;
+        private void Awake()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        public void MoveToPoint(Vector3 point)
+        {
+            _agent.SetDestination(point);
+        }
+
+        private void Update()
+        {
+            if (Keyboard.current.tKey.wasPressedThisFrame)
+                _conversation.Speech(testLineType);
+        }
+    }
+}
---
+++ Assets/Works/JW/Code/NPC/NPCConversationCompo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Code.NPC
+{
+    [Serializable]
+    public struct NPCLine
+    {
+        public string lineType;
+        public string line;
+        
+        public NPCLine(string lineType, string line)
+        {
+            this.lineType = lineType;
+            this.line = line;
+        }
+    }
+    
+    public class NPCConversationCompo : MonoBehaviour
+    {
+         [SerializeField] private NPCLine[] npcLines;
+         [SerializeField] private TextMeshProUGUI ui;
+         public void Speech(string lineType)
+         {
+             var lines = npcLines.Where(line => line.lineType == lineType).ToArray();
+                 
+             if (lines.Length > 0)
+             {
+                int idx = Random.Range(0, lines.Length);
+                
+                ShowTextUI(lines[idx].line);
+             }
+             
+         }
+
+         private void ShowTextUI(string text)
+         {
+             Debug.Log(text);
+             //StringBuilder << 얘도 아쉽다=.
+             ui.maxVisibleCharacters = 10; // 나중에 다시 공부해
+             
+         }
+    }
+}
