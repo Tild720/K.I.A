a753b6b99e675b5a884566644c50d4373ef7b1c3
K-W-J <ljohappyday@gmail.com>
2025-09-22 23:01:33 +0900

    feat : Pot and Oven
    feat : Door Script
    feat : LightToggle Script
    faet : LookAtCamera Script

---
+++ Assets/Works/KWJ/01_Code/Etc/LookAtCamera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace KWJ.Etc
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        private Camera _camera;
+        
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            transform.LookAt(_camera.transform);
+        }
+    }
+}
