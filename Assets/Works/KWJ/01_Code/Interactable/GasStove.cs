8d027cb2501082255b6dd211ca9029cbbb53f6eb
K-W-J <ljohappyday@gmail.com>
2025-09-29 16:09:28 +0900

    feat : FoodQuality
    chore : Personajes
    feat : FoodQuality Script

---
+++ Assets/Works/KWJ/01_Code/Interactable/GasStove.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Works.KWJ._01_Code.Interactable
+{
+    public class GasStove : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Works/KWJ/01_Code/Interactable/PickUpableHolder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Works.KWJ._01_Code.Interactable
+{
+    public class ItemHolder : MonoBehaviour
+    {
+        [SerializeField] private Transform _container;
+
+        private void HoldItem(Transform target)
+        {
+            
+        }
+    }
+}
