f5af9f7af96c395d4383f7b01712e520ca4cabeb
jiwon0612 <iololloli9976@gmail.com>
2025-09-29 23:26:42 +0900

    feat:FoodEvents

---
+++ Assets/Works/JW/Events/FoodEvents.cs
@@ -0,0 +1,23 @@
+﻿using Code.Core.EventSystems;
+using KWJ.Define;
+
+namespace Works.JW.Events
+{
+    public static class FoodEvents
+    {
+        public static readonly FoodEatEvent FoodEatEvent = new FoodEatEvent();
+    }
+
+    public class FoodEatEvent : GameEvent
+    {
+        public FoodType foodType;
+        public FoodState foodState;
+
+        public FoodEatEvent Init(FoodType foodType, FoodState foodState)
+        {
+            this.foodState = foodState;
+            this.foodType = foodType;
+            return this;
+        }
+    }
+}
