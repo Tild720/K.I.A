9ffd8298047d5fe201cb57ebbebb59c7f0f56f45
K-W-J <ljohappyday@gmail.com>
2025-09-30 19:27:07 +0900

    feat : FeedBack and SoundFeedBack

---
+++ Assets/Works/KWJ/01_Code/FeedBacks/SoundFeedBack.cs
@@ -0,0 +1,20 @@
+﻿using Ami.BroAudio;
+using UnityEngine;
+
+namespace KWJ.FeedBacks
+{
+    public class SoundFeedBack : FeedBack
+    {
+        [SerializeField] private SoundID soundID;
+        
+        public override void CreateFeedback()
+        {
+            BroAudio.Play(soundID);
+        }
+
+        public override void StopFeedback()
+        {
+            
+        }
+    }
+}
