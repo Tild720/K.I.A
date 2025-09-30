abc1fe6c604f49d3c4b6711abc688db3f695c088
K-W-J <ljohappyday@gmail.com>
2025-09-30 17:45:36 +0900

    feat : IngredientManager

---
+++ Assets/Works/KWJ/01_Code/Core/EventSystem/GetCookingToolEvent.cs
@@ -0,0 +1,23 @@
+﻿using Code.Core.EventSystems;
+using Foods;
+using KWJ.Interactable.PickUpable;
+
+namespace Core.EventSystem
+{
+    public static class GetCookingToolEvents
+    {
+        public static GetCookingToolEvent GetCookingToolEvent = new GetCookingToolEvent();
+    }
+    public class GetCookingToolEvent : GameEvent
+    {
+        public PickUpable pickUpable;
+        public int count;
+
+        public GetCookingToolEvent Initialize(PickUpable pickUpable, int count)
+        {
+            this.pickUpable = pickUpable;
+            this.count = count;
+            return this;
+        }
+    }
+}
--- Assets/Works/KWJ/01_Code/Manager/IngredientManager.cs
+++ Assets/Works/KWJ/01_Code/Manager/IngredientManager.cs
@@ -1,40 +1,46 @@
 ﻿using System.Collections.Generic;
 using Code.Core.EventSystems;
 using Core.EventSystem;
-using Foods;
 using KWJ.Core;
 using KWJ.Interactable.PickUpable;
 using UnityEngine;
+using Works.JW.Events;
 
 namespace KWJ.Manager
 {
-    public class ItemManager : MonoSingleton<ItemManager>
+    public class IngredientManager : MonoSingleton<IngredientManager>
     {
         [SerializeField] private List<Ingredient> ingredients = new List<Ingredient>();
         [SerializeField] private Transform itemSpawnPoint;
         private void OnEnable()
         {
             GameEventBus.AddListener<PurchaseEvent>(CreateItem);
-            GameEventBus.AddListener<GetItemEvent>(AddItem);
+            GameEventBus.AddListener<NPCLineEndEvent>(ResetIngredients);
         }
         
         private void OnDisable()
         {
             GameEventBus.RemoveListener<PurchaseEvent>(CreateItem);
-            GameEventBus.RemoveListener<GetItemEvent>(AddItem);
-        }
-        
-        private void AddItem(GetItemEvent evt)
-        {
-            ingredients.Add(evt.ingredient);
+            GameEventBus.RemoveListener<NPCLineEndEvent>(ResetIngredients);
         }
 
+        private void ResetIngredients(NPCLineEndEvent evt)
+        {
+            foreach (var ingredient in ingredients)
+            {
+                Destroy(ingredient.gameObject);
+            }
+            
+            ingredients.Clear();
+        }
+        
         private void CreateItem(PurchaseEvent evt)
         {
             for (int i = 0; i < evt.count; i++)
             {
                 GameObject item = Instantiate(evt.food.foodPrefab, itemSpawnPoint.position, Quaternion.identity);
-                //ingredients.Add(item.GetComponent<PickUpable>());
+                Ingredient ingredient = item.GetComponentInChildren<Ingredient>();
+                ingredients.Add(ingredient);
             }
         }
     }
