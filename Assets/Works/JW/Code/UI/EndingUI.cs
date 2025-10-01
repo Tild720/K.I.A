9ba66fc6f7eacac0b09f09e3ba916c85780602f6
jiwon0612 <iololloli9976@gmail.com>
2025-10-01 23:26:42 +0900

    feat:EndingScene

--- Assets/Works/JW/Code/UI/EndingUI.cs
+++ Assets/Works/JW/Code/UI/EndingUI.cs
@@ -1,9 +1,50 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Works.JW.Code.UI
 {
     public class EndingUI : MonoBehaviour
     {
+        [SerializeField,TextArea] private string mainLine;
+        [SerializeField] private float mainLineAnimationTime;
+        [SerializeField] private TextMeshProUGUI mainTextUI;
+        [SerializeField] private Image image;
+
+        private WaitForSeconds _mainWait;
         
+        private void Awake()
+        {
+            _mainWait = new WaitForSeconds(mainLineAnimationTime);
+            image.gameObject.SetActive(false);
+
+            StartCoroutine(TextAnimation(mainTextUI, mainLine, _mainWait, () =>
+            {
+                image.gameObject.SetActive(true);
+            }));
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+            Debug.Log("나가짐");
+        }
+
+        private IEnumerator TextAnimation(TextMeshProUGUI ui, string line, WaitForSeconds wait, Action endCallback = null)
+        {
+            ui.SetText(line);
+            ui.maxVisibleCharacters = 0;
+            
+            for (int i = 0; i < line.Length; i++)
+            {
+                yield return wait; 
+                ui.maxVisibleCharacters++;
+            }
+
+            yield return wait;
+            endCallback?.Invoke();
+        }
     }
 }
