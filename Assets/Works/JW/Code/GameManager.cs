2307e3831a29a910b20a67427dddb108c83761a7
jiwon0612 <iololloli9976@gmail.com>
2025-10-02 00:29:24 +0900

    feat:GameManager

---
+++ Assets/Works/JW/Code/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Works.JW.Code
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else
+                Destroy(instance.gameObject);
+        }
+
+        public void SetCursor(bool isVis)
+        {
+            Cursor.visible = isVis;
+            Cursor.lockState = isVis ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+    }
+}
